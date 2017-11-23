e746c78e61bc2ff49ff9e67186a5c1cf5ee45e53
DESKTOP-0R41BDV\Riad133 <riad.nazrul@gmail.com>
2017-11-24 00:10:34 +0600

    Add AutoMapper & Create Mapping Profile class and initial it in Globel.asax.cs file

---
+++ PMS.WebApi/App_Start/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using PMS.WebApi.Dtos;
+using PMS.WebApi.Models;
+
+namespace PMS.WebApi.App_Start
+{
+    public class MappingProfile:Profile
+    {
+        public MappingProfile()
+        {
+            Mapper.CreateMap<Product, ProductDto>();
+            Mapper.CreateMap<ProductDto, Product>();
+        }
+    }
+}
--- PMS.WebApi/Dtos/ProductDto.cs
+++ PMS.WebApi/Dtos/ProductDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,19 @@ namespace PMS.WebApi.Dtos
 {
     public class ProductDto
     {
+        public int ProductId { get; set; }
+        public string ProductCode { get; set; }
+
+        [MaxLength(265)]
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+
+        [MaxLength(256)]
+        public string ProductName { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public string ImageUrl { get; set; }
+
     }
 }
