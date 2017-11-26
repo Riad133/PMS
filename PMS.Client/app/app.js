e5e1b2983a0a26b4ef16a1592bb37de245b20d34
DESKTOP-0R41BDV\Riad133 <riad.nazrul@gmail.com>
2017-11-26 17:29:00 +0600

    Add UI Router and Config Defaul route using Angular Ui Router

--- PMS.Client/app/app.js
+++ PMS.Client/app/app.js
@@ -1,4 +1,26 @@
-﻿(function() {
+﻿
+(function() {
     "use strict";
-    var app = angular.module("productManagement", ["common.services"]);
+    var app = angular.module("productManagement",
+                             ["common.services",
+                                 "ui.router"
+                              
+                            ]);
+
+    app.config(["$stateProvider", "$urlRouterProvider", function ($stateProvider, $urlRouterProvider) {
+
+        // default route
+       // $qProvider.errorOnUnhandledRejections(false);
+        $urlRouterProvider.otherwise("/products");
+        $stateProvider
+            .state("productList",
+                {
+                    url: "/products",
+                    templateUrl: "app/products/productListView.html",
+                    controller: "ProductListViewCtrl as vm"
+                });
+
+      
+    }]);
+
 }());
